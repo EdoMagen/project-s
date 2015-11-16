cecdb246c05c7e94cb903e1f22f11f87a63f8435
edocomo <edomagen@Edos-MacBook-Pro.local>
2015-11-16 12:49:35 +0200

    testing quick commit

--- js/show.js
+++ js/show.js
@@ -175,6 +175,7 @@ function build_tree() {
         else {
             ig_media = '<img src="' + payload.instagram[ig_counter].images.standard_resolution.url + '">'
             ig_arr.push(payload.instagram[ig_counter].images.standard_resolution.url);
+            
         }
         var ig_wrapper = document.createElement('div');
         ig_wrapper.className += "ig_wrapper";
